35e1f19a2b4bc176f1e25faf557553b0cf95a66e
Paul Kirkpatrick <RadicalTurnip@gmail.com>
2020-10-04 23:42:13 -0500

    question card set up and centered. Still need buttons and to be filled with questions.

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 // change timeLeft to change how much time is on the starting timer
-var timeLeft = 100
+var timeLeft = 100;
 
 // The array that holds the questions should always be strings in this order:
 //[question, right answer, wrong answer, wrong answer, wrong answer]
-// the Javascript chest for "all of the above" answers and moves them to the bottom, but otherwise randomizes answers. 
+// the Javascript checks for "all of the above" answers and moves them to the bottom, but otherwise randomizes answers. 
 // Still put the correct answer as object[1] in the array.
-var question1 = ["What does HTML stand for?", "HyperText Markup Language", "Hollistic Text and Menu Language", "HyperText Menu Language", "Hippocampus, Thyroid, Metacarpals, and Lymphatic systems"]
-var question2 = ["I want to change the color scheme of my wepage. Which would I most likely need to change?", "The CSS", "The HTML", "The Javascript", "The DOM"]
-var question3 = ["In the DOM, I'm able to inspect which of the following?", "All of the above", "The style of elements", "The HTML of elements", "Event Listeners"]
-var question4 = ["What is Bootstrap's design model?", "Mobile first", "Rounded corners", "Descending order of importance", "Make it obvious"]
+var question1 = ["What does HTML stand for?", "HyperText Markup Language", "Hollistic Text and Menu Language", "HyperText Menu Language", "Hippocampus, Thyroid, Metacarpals, and Lymphatic systems"];
+var question2 = ["I want to change the color scheme of my wepage. Which would I most likely need to change?", "The CSS", "The HTML", "The Javascript", "The DOM"];
+var question3 = ["In Chrome Dev Tools, I'm able to inspect which of the following?", "All of the above", "The HTML", "The Javascript", "Event Listeners"];
+var question4 = ["What is Bootstrap's design model?", "Mobile first", "Rounded corners", "Descending order of importance", "Make it obvious"];
 
-var jumbotron = document.querySelector(".jumbotron")
-var startButton = document.getElementById("start-button")
+var jumbotronEl = document.querySelector(".jumbotron");
+var startButtonEl = document.getElementById("start-button");
 
 
-function createQuestion1() {
-  jumbotron.remove
+function doQuestion1() {
+  jumbotronEl.remove();
+  var questionContainer = document.createElement("div");
+  var questionCard = document.createElement("div");
+  var cardBody = document.createElement("div");
+  var cardTitle = document.createElement("h5");
+  var questionValue = document.createElement("p");
+  questionContainer.setAttribute("class", "d-flex justify-content-center");
+  questionCard.setAttribute("class", "card");
+  questionCard.setAttribute("style", "width: 55rem;");
+  cardBody.setAttribute("class", "card-body");
+  cardTitle.setAttribute("class", "card-title");
+  cardTitle.textContent = "Question:";
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(questionValue);
+  questionCard.appendChild(cardBody);
+  questionContainer.appendChild(questionCard);
+  document.body.appendChild(questionContainer);
+};
 
-}
-
-startButton.addEventListener("click", question1)
+startButtonEl.addEventListener("click", doQuestion1);
